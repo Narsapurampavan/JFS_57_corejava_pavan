3888c31edf9771b08cf981f0271541997cc52bec
PAVAN <narsapurampavan@gmail.com>
2026-08-28 21:59:44 +0530

    Implement array input and output in Array1D
    Added functionality to input and display array elements.

--- Array1D.java
+++ Array1D.java
@@ -3,7 +3,25 @@ package codegananDSA;
 public class Array1D {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		    Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+		
 
 	}
 
